be5b04a0361cd0aa58398065f33a8e51c72c24fb
kimhaun <wha02068@gmail.com>
2020-11-02 13:34:53 +0900

    기본세팅

---
+++ app/src/main/java/kr/co/haun/myfishbowlapplication/ViewFishDetailActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.haun.myfishbowlapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewFishDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_fish_detail)
+    }
+}
